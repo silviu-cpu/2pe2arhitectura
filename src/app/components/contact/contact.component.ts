a7e68950a5d48518e13145350efe6df9471797dd
Silviu Barbu <s.barbu@goit.global>
2024-10-29 19:41:12 +0200

    Added project structure and email

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  sendEmail(e: Event) {
+    e.preventDefault();
 
+    emailjs
+      .sendForm(
+        'service_xd827el', 
+        'template_wc5657i',
+        e.target as HTMLFormElement,
+        'user_EelcXR1O2dtBCcKFTmS2V'
+      )
+      .then(
+        (result: EmailJSResponseStatus) => {
+          console.log('Email successfully sent!', result.text);
+          this.clearForm();
+        },
+        (error) => {
+          console.error('Error sending email:', error.text);
+        }
+      );
+  }
+
+  clearForm() {
+    const form = document.querySelector('form') as HTMLFormElement;
+    form.reset();
+  }
 }
--- src/app/components/projects/project-one/project-one.component.ts
+++ src/app/components/projects/project-one/project-one.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./project-one.component.scss'],
 })
 export class ProjectOneComponent {
-  // Any specific logic for Project One can go here
+  photos = [
+    { src: './../../../assets/projectone.jpg', alt: 'Project Image 1' },
+    { src: './../../../assets/projectone2.png', alt: 'Project Image 2' },
+    { src: './../../../assets/projectone3.jpg', alt: 'Project Image 3' },
+    { src: './../../../assets/projectone4.jpg', alt: 'Project Image 4' },
+    { src: './../../../assets/projectone5.jpg', alt: 'Project Image 5' }
+  ];
 }
